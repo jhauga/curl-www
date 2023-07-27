7bb1abad903f2ca3854a5101576ae65464fbcf1d
John Haugabook <johnhaugabook@gmail.com>
2023-07-26 20:21:46 -0400

    curl-www: add responsive manpage option menu
    Updated js and css to be more responsive and user friendly. 
    Edited option menu to incorportate those changes.
    Should be last local commit before making feature request in pull.

--- search-manpage.js
+++ search-manpage.js
@@ -23,23 +23,87 @@
 *  in this Software without prior written authorization of the copyright holder.
 **********************************************************************************/
 
-var setScroll = 0;
+// on/off switch for top button, menu, mouseover, and button status respectively
+var setScroll = 0; var scrollOver100 = 0; var mousedOver = 0; var onOffButton = 0;
+
+// Option menu DOM elements
+var toTop = document.getElementById("toTop"); // to top button
+var searchButton = document.getElementById("searchButton"); // show option dropdown
+var searchButtonData = searchButton.dataset; // switch for button on mouseover
+var manpageOptionMenu = document.getElementById("manpage-option-menu"); // top of DOM
+var manpageOptionMenuData = manpageOptionMenu.dataset; // switch to put menu to side
 
 // Show to top button.
-function showTop(clicked) {
-  if (clicked == undefined) clicked = 0;
-  let toTop = document.getElementById("toTop");
-  if (clicked == 1) toTop.style.display = "inline-block";
-  if (setScroll == 0) {
-    setScroll = 1;
-    document.addEventListener('scroll', function() {
-      if (document.documentElement.scrollTop > 500) {
-       toTop.style.display = 'inline-block';
-      } else {
-       toTop.style.display = 'none';
+function showTop() {  
+    let curScroll, nextScroll;
+    document.addEventListener("scroll", function() {
+      // switch for scrolled down over 100
+      if (document.documentElement.scrollTop > 100) { scrollOver100 = 1; }
+      else { scrollOver100 = 0; } 
+      
+      // Toggle button to side and stay
+      if ( // scrolled past 100px, menu is not on side, and not moused over
+        scrollOver100 == 1 &&
+        manpageOptionMenuData.onSide == 0 &&
+        mousedOver == 0 && 
+        onOffButton == 0
+       ) {       
+       manpageOptionMenu.dataset.onSide = 1;
+       if ( searchButton.className.indexOf("searchButtonToSide") > -1 &&
+            manpageOptionMenuData.toSide == 1 ) {
+          // put menu to side and done          
+          searchButton.dataset.toSide = 1;
+       }
       }
-    });
+      else if (document.documentElement.scrollTop > 500) {       
+        // show to top button on right
+        toTop.style.display = "inline-block";       
+      } else {
+       // hide to top button
+       toTop.style.display = "none"; // hide to top       
+       searchButton.dataset.toSide = 0; // button default
+       
+       // set menu to defaults
+       if (document.documentElement.scrollTop < 100) {
+         manpageOptionMenu.style.left = ""; // now use css styles
+         manpageOptionMenu.dataset.onSide = 0;
+       }
+      }
+    });  
+}
+
+// Put to top and show option buttons to side when menu not in view
+// adding mouseover function to activate.
+function buttonToSide(status, onoff) {
+  // if on exit    
+  if (
+   ( onoff == 1 || onOffButton == 1 ) &&
+     onoff != "on"
+    ) { mousedOver = 1; return; }
+  // touch event occured
+  if (onoff == "off") { searchButton.dataset.touchStatus = "on"; }
+  else { searchButton.dataset.touchStatus = "off"; }
+  // is moused over or not. exit on mouse out  
+  if ( status == 1 ) { 
+    mousedOver = 1;
+  } else { 
+    mousedOver = 0;
+    // better ux with timeout
+    setTimeout(function() { return; }, 500);     
   }
+
+  if ( // scroll switch is set and menu is on side
+    scrollOver100 == 1 && 
+    manpageOptionMenuData.onSide == 1
+   ) {     
+     manpageOptionMenu.dataset.onSide = 0; // position with css
+   } 
+   else if ( // scroll sitch is set and menu not on side
+     scrollOver100 == 1 &&
+     manpageOptionMenu.dataset.onSide == 0 // left 0px with css
+      ) {
+     manpageOptionMenu.dataset.onSide = 1; // left -80px with css
+   }
 }
 
 // Search for options in dropdown.
@@ -58,28 +122,48 @@ function searchOptions(txt) {
 
 // Show and hide table of contents
 function toggleSearchButton(showhide, onoff) {
+  // responsive elements
   let manpageDiv = document.getElementsByClassName("manpage")[0];
-  let manpageMenu = document.getElementsByClassName("menu")[0];
-  let showItem = showhide.nextElementSibling;
-  let curData = onoff.dataset;
-  if (curData.onoff == 0) {
+  let manpageMenu = document.getElementsByClassName("menu")[0];  
+  
+  // toggle display elements
+  let showItem = showhide.nextElementSibling; // --------------------\ select option menu
+  let curData = onoff.dataset; //                                    |
+  if (curData.onoff == 0) { //                                      \|/
+    // 1.set switch, show menu, change style, change text         1  .
+    manpageOptionMenu.dataset.toSide = 0; 
     curData.onoff = 1;
     showItem.style.display = "";
     onoff.style.background = "white";
-    onoff.style.color = "black";
-    onoff.innerHTML = onoff.innerHTML.replace("Show", "Hide");
-    // responsive margin to page if screen below 770
-    if (!manpageDiv.id) { manpageDiv.id = "activeManDiv"; }
-    if (!manpageMenu.id) { manpageMenu.id = "activeManMenu"; }
+    onoff.style.color = "black"; //                                  |
+    onoff.innerHTML = onoff.innerHTML.replace("Show", "Hide"); //   \|/                                                                 
+    // 2. set to side when menu out of view and style parrent     2  .
+    onoff.className = ""; 
+    onoff.parentElement.className = onoff //                         |
+    .parentElement.className.replace(" inactive", ""); //           \|/
+    // 3. responsive margins if screen < 770 add else remove       3 .
+    if (!manpageDiv.id) { manpageDiv.id = "activeManDiv"; } 
+    if (!manpageMenu.id) { manpageMenu.id = "activeManMenu"; }     
+    // Turn button on
+    onOffButton = 1;
   } else {
-    curData.onoff = 0;
+    // "..."                                                       1 .
+    manpageOptionMenu.dataset.toSide = 1; //                        /|\
+    curData.onoff = 0; //                                            |
     showItem.style.display = "none";
-    onoff.style.background = "";
+    onoff.style.background = "";    
     onoff.style.color = "";
-    onoff.innerHTML = onoff.innerHTML.replace("Hide", "Show");
-    // remove id from div.manpage
-    if (manpageDiv.id) { manpageDiv.removeAttribute("id");}
-    if (manpageMenu.id) { manpageMenu.removeAttribute("id");}
+    onoff.innerHTML = onoff.innerHTML.replace("Hide", "Show"); 
+    // "..."                                                       2 .
+    onoff.className = "searchButtonToSide"; //                      /|\
+    onoff.parentElement.className += " inactive"; //                 |
+    // "..."                                                       3 .
+    if (manpageDiv.id) { manpageDiv.removeAttribute("id");} //      /|\
+    if (manpageMenu.id) { manpageMenu.removeAttribute("id");} //     |    
+    //                                                               |
+    // put back to side if scrolled past 100. -----------------------/
+    // Turn button off
+    onOffButton = 0;
   }
 }
 
@@ -88,64 +172,34 @@ function addOptionLinks() {
   let optionMenu = document.getElementById("optionMenu");
   let optionMenuLi = optionMenu.getElementsByTagName("li");
   let fullOptionList = document.getElementById("fullOptionList");
+  
+  var lastDec, hValue;
+  var outPeriod = function() { // remove any periods    
+    if (hValue.match(/\w\d[.]/g)) {
+      lastDec = hValue.lastIndexOf(".");
+      hValue = hValue.substr(0, lastDec) + hValue.substr(Number(lastDec + 1));
+     }
+  };
+  
   fullOptionList.style.display = "";
   optionMenuLi = fullOptionList.getElementsByTagName("li");
+  
   for (i = 0; i < optionMenuLi.length; i++) {
     optionMenuLi[i].getElementsByTagName("a")[0].addEventListener("click", function() {
-      var hValue;
-      if (
-          this.innerHTML.indexOf(",") > -1 || 
-          this.innerHTML.indexOf("&lt;") > -1 ||
-        ( this.innerHTML.indexOf("[") > -1 && this.innerHTML.indexOf("]") > -1 )
-       ) {
-        if (this.innerHTML.indexOf(",") > -1 && this.innerHTML.indexOf("&lt;") > -1) {
-          if (this.innerHTML.indexOf(", --") == -1) {
-            this.href = "#" + this.innerHTML.substr(0, this.innerHTML.indexOf(" "));          
-          } else {
-            this.href = "#" + this.innerHTML.substr(0, this.innerHTML.indexOf(","));          
-          }
-         } else {
-          if (this.innerHTML.indexOf(",") > -1 && this.innerHTML.indexOf("&lt;") == -1) {
-            this.href = "#" + this.innerHTML.substr(0, this.innerHTML.indexOf(","));
-          } 
-          else if (this.innerHTML.indexOf("[") > -1 && this.innerHTML.indexOf("]") > -1) {
-            this.href = "#" + this.innerHTML.substr(0,this.innerHTML.indexOf(" "));
-          } else {
-            this.href = "#" + this.innerHTML.substr(0,this.innerHTML.indexOf("&lt;"));
-          }
-        }
-        hValue = this.href;
-        
-        // remove any periods
-        if (hValue.match(/\w\d[.]/g)) {
-          let lastDec = hValue.lastIndexOf(".");
-          hValue = 
-           hValue.substr(0, lastDec) + hValue.substr(Number(lastDec + 1));
-         } 
+      if (this.innerHTML.indexOf(",") > -1) {        
+        this.href = "#" + this.innerHTML.substr(0, this.innerHTML.indexOf(","));
+        hValue = this.href;        
       } else {
-        if (this.innerHTML.match(/\w\d[.]/g)) {
-          this.href = "#" + this.innerHTML.replace(".", "");
-          hValue = this.href;
-        } else {
-          this.href = "#" + this.innerHTML;
-          hValue = this.href;        
-        }
+        this.href = "#" + this.innerHTML;
+        hValue = this.href;              
       }
-      let hasSpace = 1;
-
-      while (hasSpace == 1) {
-        if (hValue.indexOf(" ") > -1) {
-          hValue = hValue.replace(" ", "");
-        } else {
-          hasSpace = 0;
-        }
-      }
+      outPeriod();
+      
       if (hValue.indexOf("#-#") > -1 || hValue.indexOf("#-:") > -1) {
         if (hValue.indexOf("#-#") > -1) {
           hValue = hValue.replace("#-#", "#-hash");
         }
         else {
-          //hValue = hValue.replace("#-:", "#--next");
           let skip;
         }
         this.href = hValue;
@@ -170,9 +224,6 @@ to turn on tests. In order to test the browser must support console.log.
      4. The results will be as such: 
        A. Anchor Count = number of options in menu 
        B. Option Anchors = array with name of options in menu 
-       C. Invert Count = number of otions that have invert or "no-opt-name" call. 
-       D. Option Inverts = options in menu that have an invert call.
-       E. Total Options = total number of options.
      5. Delete or set variable "testOptionAnchors" back to 0 to turn off test.
 ***********************************************************************************/
 var testOptionAnchors = 0;
@@ -180,14 +231,16 @@ var testOptionAnchors = 0;
 function testTheOptionAnchors() {
   var logSplit = function(x) {
     if (x == undefined) { x = 0; }
-    if (x == 2) { 
+    if (x == 2) {             
+      console.log("***********************************************************************"); 
+      console.log("****************************STARTING TEST******************************"); 
       console.log("\n"); 
-      console.log("*****************************************************************"); 
     } else { 
-      console.log("*****************************************************************"); 
+      console.log("\n"); 
+      console.log("****************************FINISHED TEST******************************");       
+      console.log("***********************************************************************");       
     }
   };
-  console.log(" -----------------------STARTING TEST----------------------- ");
   
   var theUnorderdOptionList = document.getElementById("optionMenu");
   var theOptionListItems = theUnorderdOptionList.getElementsByTagName("li");  
@@ -202,55 +255,7 @@ function testTheOptionAnchors() {
   console.log("A. Anchor Count - " + theOptionListItems.length);  
   console.log("B. Option Anchors:");
   console.log(optionAnchors);  
-  logSplit(); 
-  
-  // Copy contents
-  var manpageDiv = document.getElementsByClassName("manpage")[0];
-  var manpageInnerHTML = manpageDiv.innerHTML;
-  var invertCheck = document.createElement("div");
-  invertCheck.id = "iCheckForOptionsWithNo";
-  invertCheck.style.display = "none";
-  invertCheck.innerHTML = manpageInnerHTML;
-  document.body.insertAdjacentElement("afterend", invertCheck);
-  var iCheckForOptionsWithNo = document.getElementById("iCheckForOptionsWithNo");
-  var noTextContents = iCheckForOptionsWithNo.innerText;
-  let hasNo = 1; let noArray = []; let curNo;
-  while(hasNo == 1) {
-   if (noTextContents.indexOf("-no") > -1) {
-    noTextContents = noTextContents.substr(noTextContents.indexOf("-no"));
-    if (noTextContents.indexOf(" ") < noTextContents.indexOf(".")) {
-      curNo = noTextContents.substr(noTextContents.indexOf("-no"), noTextContents.indexOf(" "));
-    } else {
-      curNo = noTextContents.substr(noTextContents.indexOf("-no"), noTextContents.indexOf("."));
-    }
-    let hasNoArray = 1;
-    while (hasNoArray == 1) {
-     if (noTextContents.indexOf(curNo) > -1) {
-      noTextContents = noTextContents.replace(curNo, "");
-     } else {
-       hasNoArray = 0;
-     }
-    }
-    noArray.push(curNo);
-   } else {
-    hasNo = 0;
-   }
-  }
-  // output nos
-  logSplit(2); 
-  console.log("C. Invert Count - " + noArray.length);
-  console.log("D. Option Inverts:");  
-  console.log(noArray.sort());
-  logSplit(); 
-  
-  var totalOptions = Number(noArray.length + theOptionListItems.length);
-  // output total and remove test text
-  logSplit(2); 
-  console.log("E. Total Options - " + totalOptions);
-  logSplit(); 
-  console.log(" -----------------------FINISHED TEST----------------------- ");
-  
-  iCheckForOptionsWithNo.remove(); // delete duplicate element used to extract nos.
+  logSplit();    
 }
 
 if (testOptionAnchors == 1) { testTheOptionAnchors(); }
