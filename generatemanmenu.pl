bbe81a365d60613ee251aea0a884a418582da97d
John Haugabook <johnhaugabook@gmail.com>
2023-07-31 22:21:32 -0400

    curl-www: subtract elements

--- generatemanmenu.pl
+++ generatemanmenu.pl
@@ -1,50 +1,64 @@
 #!/usr/bin/env perl
 
-# So that the options included in the menu are
-# current with most recent version of curl.
+# Output to stdout
+my $OutManMenu = \*STDOUT;
 
-# Would probably need better process
-# to extract the option names than this
-# vodoo, but seeing as this is a feature 
-# request - hey - it's a starting point.
-
+# Make option menu current with most recent version of curl.
 # Output options to file, remove first line, and make page for option menu.
 my $curlPath = @ARGV[0];                   # store path to curl in variable
 `$curlPath/src/curl -h all > options.txt`; # output options to text file
 `sed -i 1d options.txt`;                   # remove first line in options.txt
-`touch _manpage-option-menu.html`;         # make page to hold option menu elements
 
-# Store files in variable
+# Store option file in variable
 my $_options = "options.txt"; 
-my $_optionMenuHTML = "_manpage-option-menu.html"; 
 
-# Dont' know the best way to store large text elements
-# so used html file. Below works, but whatever method you
-# guys think is best to make final menu element.
-
-# Output opening html tags to _manpage-option-menu.html
-`cat _manpage-option-menu-parent-tags.html > _manpage-option-menu.html`;
-
-# Open _manpage-option-menu.html to write to.
-open MAN_OPTIONS, ">>", $_optionMenuHTML or die $!;
+# Output opening html tags to stdout
+print  $OutManMenu <<OPENMANMENUHTML
+<!-- Option Search Menu -->
+<div id="manpage-option-menu" class="sideStay sideStayLeft inactive" 
+ data-to-side="1" data-on-side="0"> 
+<!-- On Off button -->
+  <button class="searchButtonToSide" id="searchButton" 
+   onclick="toggleSearchButton(this.nextElementSibling, this)" data-onoff="0"
+   onmouseenter="buttonToSide(1, this.dataset.onoff)" data-to-Side="1"
+   onmouseleave="buttonToSide(0, this.dataset.onoff)">Show Options</button><br>
+  <div style="display:none">
+    <div class="searchBox">
+      <label for="optionSearchField">Search Options</label><br>
+      <img src="https://curl.se/logo/curl-symbol.svg" alt="curl symbol">
+      <input id="optionSearchField" name="optionSearchField" 
+       type="search" oninput="searchOptions(this.value)">
+    </div>
+    <ul id="optionMenu">
+      <!-- Start option list -->
+      <div id="fullOptionList" style="display:none">
+OPENMANMENUHTML
+;
 
 # Open options.txt to extract option names and output to _manpage-option-menu.html
 open option_text, "<:encoding(UTF-8)", $_options or die $!;  
 
-# Extract only the option text.
+# Extract only the option text and output.
 # Loop through option.txt and make "<li><a href="#">..</a></li>
 while (my $row = <option_text>) {
   chomp $row;
   $row =~ s/^[[:space:]]*([^[:space:]]*-)/\1/;                   # remove starting space
   $row =~ s/^((-.*, --[^[:space:]]+)|(^--[^[:space:]]+)).*/\1/g; # remove text after option
-  print MAN_OPTIONS "<li><a href=\"#\">$row</a></li>\n";         # append to _manpage-option-menu.html
+  print $OutManMenu "        <li><a href=\"#\">$row</a></li>\n"; # append to stdout with correc indentation
 }
 
 # Close files, add closing tags, and remove files no longer needed.
-close TEMP; close option_text;
+close option_text;
 
 # Output closing tags with _manpage-option-menu.html.
-`cat _manpage-option-menu-close-tags.html >> _manpage-option-menu.html`;
+print $OutManMenu <<CLOSEMANMENUHTML
+
+      </div>
+    </ul>
+  </div>
+</div>
+CLOSEMANMENUHTML
+;
 
 # Remove option file longer needed.
 `rm options.txt`;
